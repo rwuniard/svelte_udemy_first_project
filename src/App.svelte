8943203459877e32b45cd618fed6097c532882b0
Ronson Wuniardy <rwuniard@hotmail.com>
2022-03-20 22:54:14 -0400

    Adding content to meetupitems and use it in app.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,38 @@
 <script>
     import Header from './UI/Header.svelte';
+    import MeetupItem from './Meetups/MeetupItem.svelte';
+
+    const meetups = [
+        {
+            id: 'm1',
+            title: 'Coding bootcamp',
+            subtitle: 'learning how to code',
+            description: 'meeting on how to code',
+            imageUrl: 'http://blabla.com/image.jpg',
+            address: '3200 street, New York 21223',
+            email: 'something@meetups.com',
+        },
+        {
+            id: 'm2',
+            title: 'Swimming bootcamp',
+            subtitle: 'learning how to swim',
+            description: 'meeting on how to swim',
+            imageUrl: 'http://blabla.com/image.jpg',
+            address: '110 street, Atlanta 32203',
+            email: 'swimming@meetups.com',
+        },
+    ];
 </script>
 
 <Header />
+<section id="meetups">
+    {#each meetups as meetupitem}
+        <MeetupItem />
+    {/each}
+</section>
+
+<style>
+    #meetups {
+        margin-top: 5rem;
+    }
+</style>
