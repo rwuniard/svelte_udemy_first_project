043cfa0b91932900e01b10c083dcb4904968cfd3
Ronson Wuniardy <rwuniard@hotmail.com>
2022-03-27 21:32:59 -0400

    Adding Meetup grid

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
     import Header from './UI/Header.svelte';
-    import MeetupItem from './Meetups/MeetupItem.svelte';
+    import MeetupGrid from './Meetups/MeetupGrid.svelte';
 
     const meetups = [
         {
@@ -8,7 +8,7 @@
             title: 'Coding bootcamp',
             subtitle: 'learning how to code',
             description: 'meeting on how to code',
-            imageUrl: 'http://blabla.com/image.jpg',
+            imageUrl: 'https://cdn.pixabay.com/photo/2015/03/10/17/23/youtube-667451_1280.png',
             address: '3200 street, New York 21223',
             email: 'something@meetups.com',
         },
@@ -17,7 +17,7 @@
             title: 'Swimming bootcamp',
             subtitle: 'learning how to swim',
             description: 'meeting on how to swim',
-            imageUrl: 'http://blabla.com/image.jpg',
+            imageUrl: 'https://cdn.pixabay.com/photo/2018/04/26/16/39/beach-3352363_1280.jpg',
             address: '110 street, Atlanta 32203',
             email: 'swimming@meetups.com',
         },
@@ -25,21 +25,12 @@
 </script>
 
 <Header />
-<section id="meetups">
-    {#each meetups as meetupitem}
-        <MeetupItem
-            title={meetupitem.title}
-            subtitle={meetupitem.subtitle}
-            address={meetupitem.address}
-            imageUrl={meetupitem.imageUrl}
-            description={meetupitem.imageUrl}
-            email={meetupitem.email}
-        />
-    {/each}
-</section>
+<main>
+    <MeetupGrid {meetups} />
+</main>
 
 <style>
-    #meetups {
+    main {
         margin-top: 5rem;
     }
 </style>
---
+++ src/Meetups/MeetupGrid.svelte
@@ -0,0 +1,32 @@
+<script>
+    import MeetupItem from './MeetupItem.svelte';
+    export let meetups;
+</script>
+
+<section id="meetups">
+    {#each meetups as meetupitem}
+        <MeetupItem
+            title={meetupitem.title}
+            subtitle={meetupitem.subtitle}
+            address={meetupitem.address}
+            imageUrl={meetupitem.imageUrl}
+            description={meetupitem.description}
+            email={meetupitem.email}
+        />
+    {/each}
+</section>
+
+<style>
+    section {
+        width: 100%;
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-gap: 1rem;
+    }
+
+    @media (min-width: 768px) {
+        section {
+            grid-template-columns: repeat(2, 1fr);
+        }
+    }
+</style>
